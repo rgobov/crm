26c04e491926ab39a7840aad678ea955f076d862
rgobov <rgobov@gmail.com>
2026-01-24 17:49:11 +0200

    Update production API URL

--- lib/core/config/app_config.dart
+++ lib/core/config/app_config.dart
@@ -1,13 +1,14 @@
 /// Центральный конфигурационный файл приложения.
 class AppConfig {
-  static const bool isProduction = false;
+  static const bool isProduction = true; // Установили в true для работы на сервере
 
   // --- ТЕСТИРОВАНИЕ НА РЕАЛЬНОМ ТЕЛЕФОНЕ ---
   // true: использовать IP компьютера (для реального устройства)
   // false: использовать 10.0.2.2 (для эмулятора)
-  static const bool isMobileTest = true;
+  static const bool isMobileTest = false;
 
-  static const String productionUrl = 'https://738629.cloud4box.ru/api';
+  // Адрес вашего бэкенда в Easypanel
+  static const String productionUrl = 'https://tryneuro-backend.t6xfbd.easypanel.host/api';
   
   // IP вашего компьютера в Wi-Fi сети
   static const String computerIp = '192.168.100.5';
