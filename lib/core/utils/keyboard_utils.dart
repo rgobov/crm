596744372ba894c05274cb7d9309540ec8add63d
rgobov <rgobov@gmail.com>
2026-01-28 23:16:40 +0200

    Fix missing import in keyboard_utils

--- lib/core/utils/keyboard_utils.dart
+++ lib/core/utils/keyboard_utils.dart
@@ -1,5 +1,6 @@
 import 'dart:async';
 import 'package:flutter/foundation.dart';
+import 'package:flutter/widgets.dart'; // Добавили импорт для FocusNode
 import 'dart:js' as js;
 
 class KeyboardUtils {
