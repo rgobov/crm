b6e46b93a21e06e36b2f4231ce5d043f973fe6b6
rgobov <rgobov@gmail.com>
2026-04-08 13:52:18 +0200

    chore: remove flutter/dart files and finalize solarized theme for modals

--- android/app/src/main/kotlin/com/example/try_neuro/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.try_neuro
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- lib/core/utils/js_stub.dart
+++
@@ -1,7 +0,0 @@
-/// A stub for dart:js to allow compilation on non-web platforms.
-class JsContext {
-  bool hasProperty(Object property) => false;
-  dynamic operator [](Object property) => null;
-}
-
-final JsContext context = JsContext();
